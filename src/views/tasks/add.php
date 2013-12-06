ca875a6a393afb90522f8cddd2848fcfa65b1a02
Trevor Hebert <trevorhebert@Trevor-Heberts-MacBook-Pro.local>
2013-12-05 22:09:09 -0500

    fixed bad redirect on tasks page for add.

--- src/views/tasks/add.php
+++ src/views/tasks/add.php
@@ -4,8 +4,8 @@
     <h1> Add Task</h1>
 
     <form action="addTask" method="post">
-        <label>Task Name</label><input type="text" name="taskName" /><br />
-        <label>Task Details</label><input type="text" name="taskDetails" /><br />
+        <label>Task Name</label><input type="text" name="taskName" required /><br />
+        <label>Task Details</label><input type="text" name="taskDetails" required /><br />
         <label></label><input type="submit" name="submit" />
     </form>
 </div>
